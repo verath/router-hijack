468a37487773dd313f4dd9d18a9850ee7ccbafae
Peter Eliasson <warackta@gmail.com>
2016-03-23 00:09:11 +0100

    Added a rough filtering before fingerprinting.
    The discover function does a very rough test if the ip is a web server or
    not, but (ab)using async XHR. They are apperently really fast. Running
    against 192.168.0.0-192.168.1.255 took 1473.376ms. Nice!

--- src/index.js
+++ src/index.js
@@ -1,45 +1,82 @@
-function fingerprint(ip) {
-    return new Promise((resolve, reject) => {
-        const languageSrc = `http://${ip}/languages-en.js`;
-        const frame = document.createElement('iframe');
-        const languageScript = document.createElement('script');
+function discover(ip) {
+    return new Promise((resolve) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', `http://${ip}/`, true);
+        xhr.timeout = 1000;
+        xhr.onload = () => resolve(true);
+        // We assume onerror is called only because the request
+        // was denied for cross domain reasons. If that holds,
+        // we also know that if we get an error instead of a
+        // timeout, there is likely a web server at the ip.
+        xhr.onerror = () => resolve(true);
+        xhr.ontimeout = () => resolve(false);
+        xhr.send(null);
+    });
 
+}
+
+function fingerprint(ip) {
+    return new Promise((resolve) => {
+        let languageSrc = `http://${ip}/languages-en.js`;
+        let frame = document.createElement('iframe');
+        frame.style.display = 'none';
+
+        let done = (success) => {
+            languageScript.remove();
+            frame.remove();
+            resolve(success);
+        };
+
+        let languageScript = document.createElement('script');
         languageScript.src = languageSrc;
         languageScript.addEventListener('load', () => {
-            if (frame.contentWindow['know_href'] === "http://kbserver.netgear.com/wndr3700.asp") {
-                resolve();
-            } else {
-                reject();
-            }
+            let isMatch = (frame.contentWindow['know_href'] === "http://kbserver.netgear.com/wndr3700.asp");
+            done(isMatch);
         });
-        languageScript.addEventListener('error', reject);
+        languageScript.addEventListener('error', () => done(false));
 
         document.body.appendChild(frame);
         frame.contentWindow.document.body.appendChild(languageScript);
     });
 }
 
+// TODO: usch
+function promiseFilter(filterFunc, arr) {
+    let promises = arr.map(filterFunc);
+    return Promise.all(promises).then((filterRes) => {
+        let result = [];
+        filterRes.forEach((passed, idx) => {
+            if (passed) {
+                result.push(arr[idx]);
+            }
+        });
+        return result;
+    });
+}
 
 (function main() {
-    // Wait for document loaded
+
     window.addEventListener('load', () => {
-        const fingerprintPromises = [
-            "192.168.1.245",
-            "192.168.1.246",
-            "192.168.1.247",
-            "192.168.1.248",
-            "192.168.1.249",
-            "192.168.1.250",
-            "192.168.1.251",
-            "192.168.1.252",
-            "192.168.1.253"
-        ].map((ip) => {
-            fingerprint(ip).then(() => {
-                console.log(ip, "ok!");
-            }, () => {
-                console.log(ip, "nok!");
-            });
-        });
+        console.time('execTime');
+
+        let addresses = [];
+        for (let i = 0; i < 2; i++) {
+            for (let j = 0; j < 255; j++) {
+                addresses.push(`192.168.${i}.${j}`);
+            }
+        }
+
+        promiseFilter(discover, addresses)
+            .then((addresses) => promiseFilter(fingerprint, addresses))
+            .then((addresses) => {
+                addresses.forEach((addr) => {
+                    console.log("Found at:", addr);
+                });
+                console.timeEnd('execTime');
+            })
+            .catch((err) => {
+                console.error(err);
+            })
     });
 
     // https://en.wikipedia.org/wiki/Private_network#Private_IPv4_address_spaces
